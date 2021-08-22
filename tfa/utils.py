3329a6601bdd04d895f36d7ddc8819e724698823
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-08-23 03:42:55 +0600

    remove tokens

--- tfa/utils.py
+++ tfa/utils.py
@@ -1,8 +1,8 @@
 import os
 from twilio.rest import Client
 
-account_sid = 'ACea3e56b5d026e200c5ba7aa9f127db3b'
-auth_token = '2b5f19d9360ae512789a0323cdae1885'
+account_sid = ''
+auth_token = ''
 
 client = Client(account_sid, auth_token)
 
