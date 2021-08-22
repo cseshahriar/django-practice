0ff7d4c30e63445b0a7dcb30e46ddee1eba09bed
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-08-23 00:06:13 +0600

    tfa and users app created

--- settings/settings.py
+++ settings/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    'users.apps.UsersConfig',
+    'tfa.apps.TfaConfig',
 ]
 
 MIDDLEWARE = [
