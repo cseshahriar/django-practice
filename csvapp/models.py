100d148141da7d84293bd26e459163c525d9f54a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-08-25 20:11:47 +0600

    csvapp models created

--- csvapp/models.py
+++ csvapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class CsvModel(models.Model):
+    """ create objects from a csv file upload """
+    file_name = models.FileField(upload_to='csvapp/')
+    uploaded = models.DateTimeField(auto_now_add=True)
+    activated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"File id: {self.id}"
