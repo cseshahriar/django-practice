2771e2fd96e33f3027fdcd3c569804b0102ffd62
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-08-25 20:31:27 +0600

    sale model created

--- csvapp/admin.py
+++ csvapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CsvModel
+from .models import CsvModel, Sale
 
 admin.site.register(CsvModel)
+admin.site.register(Sale)
--- csvapp/models.py
+++ csvapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 class CsvModel(models.Model):
     """ create objects from a csv file upload """
@@ -8,3 +9,36 @@ class CsvModel(models.Model):
 
     def __str__(self):
         return f"File id: {self.id}"
+
+
+PRODUCT_CHOICES = (
+    ('TV', 'IV'),
+    ('IPAD', 'Ipad'),
+    ('PLAYSTATION', 'Playstation'),
+)
+
+class Sale(models.Model):
+    product = models.CharField(max_length=200, choices=PRODUCT_CHOICES)
+    salesman = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quantitty = models.PositiveIntegerField()
+    total = models.FloatField(blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product}-{self.quantitty}"
+
+    def save(self, *args, **kwargs):
+        price = None
+        
+        if self.product == 'TV':
+            price = 599.99
+        elif self.product == 'IPAD':
+            price = 400.00
+        elif self.product == 'PLAYSTATION':
+            price = 464.99
+        else:
+            pass
+
+        self.total = price * self.quantitty
+        super().save(*args, **kwargs)
