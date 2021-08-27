1ad9a2fbd85b0945890270515c1f1aa7244d8073
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-08-27 11:17:10 +0600

    student and teacher model created

--- djorm/models.py
+++ djorm/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.surname}'
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    classroom = models.IntegerField()
+    teacher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.surname}'
+
+
