953d897f5ed1365294adf3ae06c83207e91f0e20
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-09-02 23:27:35 +0600

    customer list pdf

--- pypdf/urls.py
+++ pypdf/urls.py
@@ -3,7 +3,7 @@ from .views import render_pdf_view, customer_render_pdf_view, CustomerListView
 
 urlpatterns = [
     path('pdf/customer/', CustomerListView.as_view(), name='customer_list'),
-    path('pdf/customer/', render_pdf_view, name='customer_pdf'),
+    path('pdf/customer/pdf', render_pdf_view, name='customer_pdf'),
     path(
         'pdf/<int:pk>/download/',
         customer_render_pdf_view,
