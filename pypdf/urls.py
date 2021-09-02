6eb13c4a0b92dc77dda2c262f0d4f7b10894975c
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-09-02 23:23:28 +0600

    single object pdf

--- pypdf/urls.py
+++ pypdf/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import render_pdf_view
+from .views import render_pdf_view, customer_render_pdf_view, CustomerListView
 
 urlpatterns = [
+    path('pdf/customer/', CustomerListView.as_view(), name='customer_list'),
     path('pdf/customer/', render_pdf_view, name='customer_pdf'),
+    path(
+        'pdf/<int:pk>/download/',
+        customer_render_pdf_view,
+        name='customer_render_pdf_view'
+    ),
 ]
--- pypdf/views.py
+++ pypdf/views.py
@@ -5,27 +5,56 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 from django.contrib.staticfiles import finders
+from django.views.generic.list import ListView
 from .models import Customer
 
+
+class CustomerListView(ListView):
+   model = Customer
+   template_name = 'pdf/main.html'
+
+def customer_render_pdf_view(request, pk, *args, **kwargs):
+   template_path = 'pdf/pdf_single.html'
+   context = {'object': Customer.objects.get(pk=pk)}
+   # Create a Django response object, and specify content_type as pdf
+   response = HttpResponse(content_type='application/pdf')
+   
+   # if download
+   # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+   # if display
+   response['Content-Disposition'] = 'filename="report.pdf"'
+
+   # find the template and render it.
+   template = get_template(template_path)
+   html = template.render(context)
+
+   # create a pdf
+   pisa_status = pisa.CreatePDF(html, dest=response)
+   # if error then show some funy view
+   if pisa_status.err:
+      return HttpResponse('We had some errors <pre>' + html + '</pre>')
+   return response
+
 def render_pdf_view(request):
-    template_path = 'pdf/customer_list.html'
-    context = {'object_list': Customer.objects.all()}
-    # Create a Django response object, and specify content_type as pdf
-    response = HttpResponse(content_type='application/pdf')
-    
-    # if download
-    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+   template_path = 'pdf/pdf.html'
+   context = {'object_list': Customer.objects.all()}
+   # Create a Django response object, and specify content_type as pdf
+   response = HttpResponse(content_type='application/pdf')
+   
+   # if download
+   # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
 
-    # if display
-    response['Content-Disposition'] = 'filename="report.pdf"'
+   # if display
+   response['Content-Disposition'] = 'filename="report.pdf"'
 
-    # find the template and render it.
-    template = get_template(template_path)
-    html = template.render(context)
+   # find the template and render it.
+   template = get_template(template_path)
+   html = template.render(context)
 
-    # create a pdf
-    pisa_status = pisa.CreatePDF(html, dest=response)
-    # if error then show some funy view
-    if pisa_status.err:
-       return HttpResponse('We had some errors <pre>' + html + '</pre>')
-    return response
+   # create a pdf
+   pisa_status = pisa.CreatePDF(html, dest=response)
+   # if error then show some funy view
+   if pisa_status.err:
+      return HttpResponse('We had some errors <pre>' + html + '</pre>')
+   return response
