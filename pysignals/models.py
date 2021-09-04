1d715212550237a84b9d9c7ab1b77fed291cff78
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-09-05 00:16:04 +0600

    car model created

--- pysignals/models.py
+++ pysignals/models.py
@@ -7,3 +7,13 @@ class Buyer(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    code = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.price}-{self.buyer}"
+         
