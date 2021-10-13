92f1bd528e12ba469452a40d8a42fa14b377b56a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-13 20:36:11 +0600

    event json response with ajax

--- event/models.py
+++ event/models.py
@@ -6,6 +6,7 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.TextField()
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.title)
