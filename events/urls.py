fc82a7b8cf8f9354191aada237f35d34d8b73f2a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-13 23:21:16 +0600

    event crud

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,7 @@ from events import views
 urlpatterns = [
     path("", views.event_home, name="event_home"),
     path("<int:pk>/", views.event_detail, name="event_detail"),
+    path("event_create/", views.event_create, name="event_create"),
+    path("<int:pk>/event_update", views.event_update, name="event_update"),
+    path("<int:pk>/event_delete", views.event_delete, name="event_delete"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from events.models import Event
 from django.http import HttpResponse
+from events.models import Event
+from events.forms import EventForm
 
 def event_home(request):
     data = {
@@ -18,3 +19,44 @@ def event_detail(request, pk):
         "event": event
     }
     return render(request, 'events/detail.html', data)
+
+
+def event_create(request):
+    return process_event_form(request)
+
+
+def event_update(request, pk):
+    try:
+        return process_event_form(request, pk)
+    except:
+        return HttpResponse("Event doesn't exists")
+
+def process_event_form(request, pk=None):
+    event = Event.objects.get(pk=pk) if pk is not None else Event()
+    print('-' * 50, event)
+
+    form = EventForm(request.POST or None, instance=event)
+    if request.method == 'POST':
+        try:
+            if form.is_valid():
+                form.save()
+            return HttpResponse("New event created!!" if event is None else "Updated event!")
+        except Exception as e:
+            return HttpResponse("Error with event processing.", e)
+
+    context = {
+        "form": form,
+        "event": event
+    }
+
+    return render(request, 'events/form.html', context)
+
+
+def event_delete(request, pk):
+    try:
+        event = Event.objects.get(pk=pk)
+        if event:
+            event.delete()
+            return HttpResponse("Event deleted!")
+    except:
+        return HttpResponse("Event doesn't exist")
