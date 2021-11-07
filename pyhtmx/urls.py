972a65660e2e112f993b2e9ff4ed53c8e45450f2
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-08 01:00:11 +0600

    tasks views

---
+++ pyhtmx/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    TaskListAddTaskView,
+    TaskListCreateView,
+    TaskListDetailView,
+    TaskListFilterView,
+    TaskListListView,
+)
+
+urlpatterns = [
+    path("filter/", TaskListFilterView.as_view(), name="tasklist-filter"),
+    path("create/", TaskListCreateView.as_view(), name="tasklist-create"),
+    path(
+        "<slug:slug>/add_task/", TaskListAddTaskView.as_view(), name="tasklist-add-task"
+    ),
+    path("<slug:slug>/", TaskListDetailView.as_view(), name="tasklist-detail"),
+    path("", TaskListListView.as_view(), name="tasklist-list"),
+]
--- pyhtmx/views.py
+++ pyhtmx/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json
+from typing import Any, Dict, List, cast
+# django import
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.http.request import HttpRequest
+from django.views.generic import CreateView, DetailView, ListView
+from django_filters.views import FilterView
+# app import
+from .filters import TaskListFilter
+from .forms import TaskListCreateForm
+from .models import TaskList
 
-# Create your views here.
+
+class TaskListListView(ListView):
+    model = TaskList
+
+    def get_context_data(self, **kwargs: Dict[str, Any]) -> Dict[str, Any]:
+        return super().get_context_data(
+            form=TaskListCreateForm(), filterset=TaskListFilter, **kwargs
+        )
+
+
+class TaskListFilterView(FilterView):
+    filterset_class = TaskListFilter
+
+
+class TaskListCreateView(CreateView):
+    model = TaskList
+    form_class = TaskListCreateForm
+    template_name = "htmx/tasklist_create_form.html"
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        task_list = form.save()
+        response = HttpResponse()
+        response["HX-Trigger"] = json.dumps(
+            {"redirect": {"url": task_list.get_absolute_url()}}
+        )
+        return response
+
+
+class TaskListDetailView(DetailView):
+    model = TaskList
+
+
+class TaskListTasksView(DetailView):
+    model = TaskList
+
+
+class TaskListAddTaskView(DetailView):
+    model = TaskList
+    template_name = "htmx/tasklist_tasks.html"
+
+    def post(
+        self, request: HttpRequest, *args: List[Any], **kwargs: Dict[str, Any]
+    ) -> HttpResponse:
+        cast(TaskList, self.get_object()).tasks.create()
+        return self.get(request, *args, **kwargs)
