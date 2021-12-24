50fdee17cd7a78d0a7f400c796c2651ac6fe8c9b
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-12-24 09:59:24 +0600

    data import with thread poll executor

--- py_threading/views.py
+++ py_threading/views.py
@@ -2,9 +2,11 @@ import random
 from django.shortcuts import render
 from django.utils.timezone import utc
 from faker import Faker
+import datetime
+from concurrent.futures import ThreadPoolExecutor
+
 from .models import *
 from .thread import CreateStudentThread
-import datetime
 
 fake = Faker()
 
@@ -12,4 +14,22 @@ def home(request):
     count = 1000
     start = datetime.datetime.now()
     CreateStudentThread(count).start() 
+    return render(request, 'threading/list.html', {'message': 'success'})
+
+def data_import(count):
+    for i in range(count):
+        print(i)
+        Student.objects.create(
+            name=fake.name(),
+            email=fake.email(),
+            address=fake.address(),
+            age=random.randint(10, 50)
+        )
+        
+def home2(request):
+    count = 1000
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future = executor.submit(data_import, count)
+        future = executor.submit(data_import, count)
+        print(future.result())
     return render(request, 'threading/list.html', {'message': 'success'})
