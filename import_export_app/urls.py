f0d8676b959ba06c2ee51a04423cea90ad260dcf
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-03-20 16:33:49 +0600

    export from views with import-export

--- import_export_app/urls.py
+++ import_export_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import export, MainView
+
 
 urlpatterns = [
-    
+    path('export/posts/', MainView.as_view(), name='export_post_main'),
+    path('<str:format>/export/posts/', export, name='export_post'),
 ]
--- import_export_app/views.py
+++ import_export_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
-# Create your views here.
+from .admin import PostResource
+
+
+class MainView(TemplateView):
+    template_name = 'import_export_app/main.html'
+
+def export(request, format):
+    post_resource = PostResource()
+    dataset = post_resource.export()
+    if format == 'csv':
+        dataset_format = dataset.csv
+    else:
+        dataset_format = dataset.xls
+
+    response = HttpResponse(dataset_format, content_type=f"text/{format}")
+    response['Content-Disposition'] = f"attachment; filename=posts.{format}"
+    return response
