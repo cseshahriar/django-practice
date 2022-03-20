160514a6fa40d7e3e3dfeef2330d6d6a62f37131
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-03-20 15:42:42 +0600

    static dir problem solved

--- settings/settings.py
+++ settings/settings.py
@@ -179,6 +179,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = os.path.join(BASE_DIR, "staticfiles")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = STATIC_DIR  # production, don't forget to run collectstatic
+STATICFILES_DIRS = [STATICFILES_DIR, ]  # development environment
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('media')
