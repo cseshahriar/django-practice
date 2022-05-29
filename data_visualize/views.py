2366284844f3b7a41ca5d09d5bcf0f6f8f76ee87
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-05-29 09:47:26 +0600

    matplotlib integrate with django

--- data_visualize/views.py
+++ data_visualize/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from .models import Sale
+from .utils import get_plot
+
+
+def main_view(request):
+    qs = Sale.objects.all()
+    x = [x.item for x in qs]
+    y = [y.price for y in qs]
+    chart = get_plot(x, y)
+    return render(request, 'data/index.html', {'chart': chart})
--- settings/urls.py
+++ settings/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('multistepform/', include('multistepform.urls')),
     path('', include('editorjsapp.urls')),
     path('', include('ajaxcrud.urls')),
+    path('', include('data_visualize.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
