1f716bb77b0fff86a5f115dc46766a2d8c325ff8
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-04-22 14:52:50 +0600

    dependency fix

--- tfa/utils.py
+++ tfa/utils.py
@@ -10,13 +10,14 @@ account_sid = TWILIO_ACCOUNT_SID
 auth_token = TWILIO_AUTH_TOKEN
 twilio_number = TWILIO_NUMBER
 
-client = Client(account_sid, auth_token)
+# client = Client(account_sid, auth_token)
 
 def send_sms(user_code, phone_number):
-    messages = client.messages.create(
-        body=f'Hi! Your user and verification code is {user_code}',
-        from_=twilio_number,
-        to=f'{phone_number}'
-    )
-    print(messages)
+    # messages = client.messages.create(
+    #     body=f'Hi! Your user and verification code is {user_code}',
+    #     from_=twilio_number,
+    #     to=f'{phone_number}'
+    # )
+    # print(messages)
+    pass
 
